fa9f9b8276c4d5103a13f3e171c15f8816c2c347
MateusGaldinoLG <mateuslguilherme@gmail.com>
2024-05-21 12:15:09 -0300

    fix: remove husky para rodar sem erros

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,5 +3,6 @@ import logger from './logger';
 import Env from './env';
 
 app.listen(Env.PORT, () => {
-  logger.info(`Server started on http://localhost:${Env.PORT}/api`);
+  console.log(`Server started on http://localhost:${Env.PORT}`)
+  // logger.info(`Server started on http://localhost:${Env.PORT}/api`);
 });
