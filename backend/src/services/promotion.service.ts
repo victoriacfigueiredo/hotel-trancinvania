659bdcf9f70d69ee3d21f65d63cf1e1e90f55d54
Maria Leticia <mleticiagaspar17@gmail.com>
2024-06-12 10:20:35 -0300

    feat: update promotion service

--- backend/src/services/promotion.service.ts
+++ backend/src/services/promotion.service.ts
@@ -1,6 +1,7 @@
 import { Promotion } from "../controllers/promotion.controller";
 import { PromotionType } from "../enums/promotion-type.enum";
 import PromotionRepository from "../repositories/promotion.repository";
+import EmailService from "./email.service";
 
 export default class PromotionService {
 
@@ -10,15 +11,23 @@ export default class PromotionService {
         this.promotionRepository = new PromotionRepository();
     }
 
-    async insertPromotion(discount: number, type: string, num_rooms?: number): Promise<{ id: number }> {
+    private preparePromotionParams(discount: number, type: string, num_rooms?: number): Promotion {
         if (type === PromotionType.LIMITE_QUARTO && num_rooms === undefined) {
-            throw new Error("É necessário informar a quantidade de quartos")
+            throw new Error('Preencha todos os campos');
         }
-        
-        const params = {discount, type, num_rooms} as Promotion;
-        
-        return await this.promotionRepository.insertPromotion(params);
+    
+        let params = { discount, type, num_rooms } as Promotion;
+    
+        if (type === PromotionType.ILIMITADA) {
+            params.num_rooms = null;
+        }
+    
+        return params;
+    }
 
+    async insertPromotion(discount: number, type: string, num_rooms?: number): Promise<{ id: number }> {
+        let params = this.preparePromotionParams(discount, type, num_rooms)
+        return await this.promotionRepository.insertPromotion(params);
     }
 
     async getAllPromotions(): Promise<Promotion[]> {
@@ -30,14 +39,7 @@ export default class PromotionService {
     }
 
     async updatePromotionById(id: number, discount: number, type: string, num_rooms?: number): Promise<void> {
-
-        
-        let params = {discount, type, num_rooms} as Promotion;
-
-        if(type !== 'ILIMITADA' && num_rooms === undefined){
-            throw new Error('É necessário informar a quantidade de quartos');
-        }
-
+        let params = this.preparePromotionParams(discount, type, num_rooms)
         await this.promotionRepository.updatePromotionById(id, params);
     }
 
