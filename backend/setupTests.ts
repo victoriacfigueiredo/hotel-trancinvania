f53aff102d4e5766552bfeebe0c2d00eef25748c
victoriacfigueiredo <victoriafigueiredo62@gmail.com>
2024-06-19 11:16:43 -0300

    fix: merge conflicts

--- backend/setupTests.ts
+++ backend/setupTests.ts
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
 import { PrismaClient } from '@prisma/client'
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
 
 import prisma from './src/database/index';
 
 jest.mock('./src/database/index', () => ({
+=======
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+import prisma from './src/database/dataprisma';
+
+jest.mock('./src/database/dataprisma', () => ({
+>>>>>>> 2cd2ee4 (feat: add reservation tests)
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }))
