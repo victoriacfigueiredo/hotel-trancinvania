2eefc052b538e095f23f18fb10015b379115cdf2
duartebianca <biancaduarte1914@gmail.com>
2024-07-06 16:32:14 -0300

    fix(home): adapt navbar size

--- frontend/src/shared/components/nav-bar.tsx
+++ frontend/src/shared/components/nav-bar.tsx
@@ -3,26 +3,52 @@ import { FaSpider, FaBed, FaUser } from "react-icons/fa";
 const hotelLogoUrl = "https://i.imgur.com/QcX5CZ7.png";
 
 export const NavBar: React.FC = () => {
-    return (
-        <Box bg="#EAEAEA" color="#000000" height="178.2px">
-            <Flex justify="space-between" align="center" p={4} height="100%">
-                <Image src={hotelLogoUrl} alt="Hotel Transilvânia Logo" height="120px" />
-                <Flex align="center" gap={80} mr="70px" fontFamily="Inter" fontWeight="700" >
-                    <Link href="#" display="flex" alignItems="center" textDecoration="none" color="#000000" fontSize="30px">
-                        <FaSpider style={{ marginRight: '8px' }} />
-                        <Text>Início</Text>
-                    </Link>
-                    <Link href="#" display="flex" alignItems="center" textDecoration="none" color="#000000" fontSize="30px">
-                        <FaBed style={{ marginRight: '8px' }} />
-                        <Text>Reservas</Text>
-                    </Link>
-                    <Link href="#" display="flex" alignItems="center" textDecoration="none" color="#000000" fontSize="30px">
-                        <FaUser style={{ marginRight: '8px' }} />
-                        <Text>Perfil</Text>
-                    </Link>
-                </Flex>
-            </Flex>
-        </Box>
-
-    )
+  return (
+    <Box bg="#EAEAEA" color="#000000" height="120px">
+      <Flex justify="space-between" align="center" p={4} height="100%">
+        <Image src={hotelLogoUrl} alt="Hotel Transilvânia Logo" height="80px" />
+        <Flex
+          align="center"
+          gap={20}
+          mr="40px"
+          fontFamily="Inter"
+          fontWeight="700"
+        >
+          <Link
+            href="#"
+            display="flex"
+            alignItems="center"
+            textDecoration="none"
+            color="#000000"
+            fontSize="24px"
+          >
+            <FaSpider style={{ marginRight: "6px" }} />
+            <Text>Início</Text>
+          </Link>
+          <Link
+            href="#"
+            display="flex"
+            alignItems="center"
+            textDecoration="none"
+            color="#000000"
+            fontSize="24px"
+          >
+            <FaBed style={{ marginRight: "6px" }} />
+            <Text>Reservas</Text>
+          </Link>
+          <Link
+            href="#"
+            display="flex"
+            alignItems="center"
+            textDecoration="none"
+            color="#000000"
+            fontSize="24px"
+          >
+            <FaUser style={{ marginRight: "6px" }} />
+            <Text>Perfil</Text>
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
