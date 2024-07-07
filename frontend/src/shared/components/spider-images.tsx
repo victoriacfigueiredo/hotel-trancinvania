07701efca869be4653b7f6bb21e4b638eeaa6726
duartebianca <biancaduarte1914@gmail.com>
2024-07-07 15:20:07 -0300

    feat: add spider and net images

---
+++ frontend/src/shared/components/spider-images.tsx
@@ -0,0 +1,32 @@
+import { Image } from "@chakra-ui/react";
+import Net from "../assets/net.png";
+import Spider from "../assets/spider.png";
+
+export const BottomLeftTopRightImages: React.FC = () => {
+  return (
+    <>
+      <Image
+        src={Net}
+        alt="Bottom Left"
+        position="absolute"
+        bottom="0"
+        left="0"
+        zIndex="0"
+        display={{ base: "none", md: "block" }} // Hide on mobile, show on desktop
+        transform="scale(0.8)" // Scale down to half size
+        transformOrigin="bottom left" // Scale from the bottom left corner
+      />
+      <Image
+        src={Spider}
+        alt="Top Right"
+        position="absolute"
+        top="120px" // Adjust this value to be below the navbar
+        right="0"
+        zIndex="0"
+        display={{ base: "none", md: "block" }} // Hide on mobile, show on desktop
+        transform="scale(0.8)" // Scale down to half size
+        transformOrigin="top right" // Scale from the top right corner
+      />
+    </>
+  );
+};
