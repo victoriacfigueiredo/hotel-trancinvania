cbfaca1dcd45bad7074e11d4e43f3d33befb9943
Maria Leticia <mleticiagaspar17@gmail.com>
2024-07-07 12:12:50 -0300

    feat: add route configuration fo PPromotion, PublishedReservation, and ReservationDetai

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateTest from "./app/home/pages/CreateTest";
 import ListTests from "./app/home/pages/ListTests";
+import { Promotion } from "./app/home/pages/Promotion";
+import { PublishedReservation } from "./app/home/pages/PublishedReservation/Register";
+import { AllPublishedReservation } from "./app/home/pages/PublishedReservation/Reservations";
+import { ReservationDetails } from "./app/home/pages/PublishedReservation/ReservationDetails";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +19,26 @@ const router = createBrowserRouter([
     path: "/tests",
     Component: ListTests,
   },
+  {
+    path: '/promotions/:reservation_id',
+    Component: Promotion,
+  },
+  {
+    path: '/promotions',
+    Component: Promotion,
+  },
+  {
+    path: '/publishedReservation',
+    Component: PublishedReservation,
+  },
+  {
+    path: '/publishedReservationList',
+    Component: AllPublishedReservation,
+  },
+  {
+    path: '/publishedReservationDetails/:reservation_id',
+    Component: ReservationDetails,
+  }
 ]);
 
 export default function App() {
