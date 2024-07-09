f57a4a6048742826ff0af4b8ff1450516d5ab9c0
duartebianca <biancaduarte1914@gmail.com>
2024-07-09 10:04:00 -0300

    feat: create resend password first page

---
+++ frontend/src/app/auth/pages/resetPassword/reset_newPassword/index.tsx
@@ -0,0 +1,15 @@
+import { Box} from "@chakra-ui/react";
+import React from "react";
+import { BottomLeftTopRightImages } from "../../../../../shared/components/spider-images";
+import { NavBar } from "../../../../../shared/components/nav-bar";
+
+
+
+export const resetNewPassword: React.FC = () => {
+  return (
+    <Box bg="#191919" color="white" minH="100vh" fontFamily="Inter, sans-serif">
+  <NavBar />
+  <BottomLeftTopRightImages />
+  </Box>
+  );
+};
