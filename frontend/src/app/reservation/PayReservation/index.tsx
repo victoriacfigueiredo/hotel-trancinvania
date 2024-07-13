2c3a090629c78d3b5c7e4b5ca768e6b50df568e0
victoriacfigueiredo <victoriafigueiredo62@gmail.com>
2024-07-13 15:38:43 -0300

    feat: update PayReservation page

--- frontend/src/app/reservation/PayReservation/index.tsx
+++ frontend/src/app/reservation/PayReservation/index.tsx
@@ -1,43 +1,72 @@
-import React, { useState, ForwardedRef } from 'react';
-import { JustSpider } from "../../../shared/components/just-spider";
-import {
-  Box,
-  Text,
-  Button,
-  Icon,
-  ButtonGroup,
-  Stack,
-  HStack,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-} from '@chakra-ui/react';
-import {
-  FaArrowLeft,
-  FaArrowRight,
-  FaCalendarAlt,
-  FaWifi,
-  FaCar,
-  FaCoffee,
-  FaSnowflake,
-  FaConciergeBell,
-} from 'react-icons/fa';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import React, { useState } from 'react';
+import { JustSpider } from '../../../shared/components/just-spider';
+import { Box, Text, Stack, Icon, HStack, FormControl, FormLabel, Button, ButtonGroup } from '@chakra-ui/react';
+import { FaArrowLeft, FaCheck, FaWifi, FaCar, FaCoffee, FaSnowflake, FaConciergeBell } from 'react-icons/fa';
+import Select, { SingleValue } from 'react-select';
 import { NavBar } from '../../../shared/components/nav-bar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+interface OptionType {
+  value: string;
+  label: string;
+}
 
+const PayReservation: React.FC = () => {
+  const [selectedPayment, setSelectedPayment] = useState<SingleValue<OptionType>>(null);
 
-const App: React.FC = () => {
+  const goBack = async () => {
+    window.location.href = "http://localhost:3000/create-reservation";
+  };
+
+  const handlePayment = async () => {
+    if (!selectedPayment || selectedPayment.value === 'nothing') {
+      toast.warning("Preencha todos os campos!");
+      return;
+    }
+    toast.success("Reserva realizada com sucesso!", {
+      autoClose: 3000, // 3000ms = 3 seconds
+      onClose: () => {
+        window.location.href = "http://localhost:3000/my-reservations";
+      },
+    });
+  };
+
+  const options: OptionType[] = [
+    { value: 'nothing', label: '' },
+    { value: 'visa', label: 'Visa' },
+    { value: 'mastercard', label: 'Mastercard' },
+    { value: 'nubank', label: 'Nubank' }
+  ];
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px solid #EAEAEA',
+      color: '#191919',
+      backgroundColor: state.isFocused ? '#EAEAEA' : '#FFF',
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#EAEAEA'
+    }),
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: '#6A0572',
+      border: '1px solid #EAEAEA',
+      color: '#EAEAEA'
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      color: '#EAEAEA'
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: '#EAEAEA',
+      color: '#191919'
+    })
+  };
+
   return (
     <Box width="100vw" height="100vh" display="flex" flexDirection="column">
       <NavBar />
@@ -49,7 +78,7 @@ const App: React.FC = () => {
         alignItems="center"
         position="relative"
       >
-        <JustSpider />
+         <JustSpider />
         {/* Bloco roxo com sombra */}
         <Box position="absolute" left="200px" top="100px" width="500px">
           <Box
@@ -75,7 +104,7 @@ const App: React.FC = () => {
             fontWeight="bold"
             textAlign="center"
           >
-            Quarto Zumbi Digital
+            Zumbi Digital
           </Text>
           <Text color="#EAEAEA" fontSize="xl" textAlign="center">
             R$ 1200,00 a diária
@@ -114,11 +143,81 @@ const App: React.FC = () => {
           </HStack>
         </Box>
 
-        
+        <Box
+          position="relative"
+          width="400px"
+          height="auto"
+          border="2px solid #EAEAEA"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          left="300px"
+          py="20px"
+          top="0"
+        >
+          <Text
+            position="absolute"
+            top="-10px"
+            bg="#191919"
+            px="10px"
+            color="#EAEAEA"
+          >
+            Pagamento
+          </Text>
+          <Stack spacing={4} mt="30px" align="center" width="80%">
+            <Text color="#EAEAEA" fontSize="2xl" textAlign="center">
+              Valor total: R$ 3600,00
+            </Text>
+            <FormControl id="paymentMethod" isRequired>
+              <FormLabel color="#EAEAEA">Método de Pagamento</FormLabel>
+              <Select
+                styles={customStyles}
+                options={options}
+                placeholder=""
+                onChange={(option) => setSelectedPayment(option)}
+              />
+            </FormControl>
+            <Button
+              bg="transparent"
+              color="#EAEAEA"
+              border="1px solid #EAEAEA"
+              width="100%"
+              _hover={{ bg: '#EAEAEA', color: '#191919' }}
+            >
+              Cadastrar método de pagamento
+            </Button>
+          </Stack>
+          {/* Botões de navegação */}
+          <ButtonGroup spacing="6" mt="30px">
+            <Button
+              bg="transparent"
+              color="#EAEAEA"
+              border="1px solid #EAEAEA"
+              _hover={{ bg: '#EAEAEA', color: '#191919' }}
+              leftIcon={<Icon as={FaArrowLeft} />}
+              width="146px"
+              height="50px"
+              onClick={goBack}
+            >
+            </Button>
+            <Button
+              bg="transparent"
+              color="#EAEAEA"
+              border="1px solid #EAEAEA"
+              _hover={{ bg: '#0097B2', color: '#EAEAEA' }}
+              rightIcon={<Icon as={FaCheck} />}
+              width="146px"
+              height="50px"
+              onClick={handlePayment}
+            >
+            </Button>
+          </ButtonGroup>
+        </Box>
       </Box>
-      <ToastContainer theme="dark"/>
+      <ToastContainer theme="dark" />
     </Box>
   );
 };
 
-export default App;
+export default PayReservation;
