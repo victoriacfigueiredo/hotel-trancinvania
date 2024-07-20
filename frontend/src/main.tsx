273acda58ce06fe7b2f385493d31452f8a362fee
duartebianca <biancaduarte1914@gmail.com>
2024-07-20 18:50:35 -0300

    chore: update react-hook-form to version 7.52.1

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,9 @@ import "./global.css";
 import Provider from "./Provider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  //<React.StrictMode>
+  <Provider>
+    <App />
+  </Provider>
+  //</React.StrictMode>
 );
