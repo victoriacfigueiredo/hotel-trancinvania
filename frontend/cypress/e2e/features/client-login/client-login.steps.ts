5d366ddec0efd83550b79446c539319083d86614
duartebianca <biancaduarte1914@gmail.com>
2024-07-23 00:33:46 -0300

    test: add E2E tests for client login feature

---
+++ frontend/cypress/e2e/features/client-login/client-login.steps.ts
@@ -0,0 +1,36 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("eu estou na página {string}", (page: string) => {
+  cy.visit(page);
+});
+
+When(
+  "eu preencho o campo {string} com {string}",
+  (field: string, value: string) => {
+    cy.get(`[data-cy="${field}"]`).type(value);
+  }
+);
+
+When("eu seleciono {string}", (button: string) => {
+  cy.get(`[data-cy="${button}"]`).click();
+});
+
+Then(
+  "eu vejo um toast de sucesso com a mensagem {string}",
+  (message: string) => {
+    cy.wait(3000);
+    cy.get(".Toastify__toast-body").should("contain.text", message);
+  }
+);
+
+Then("eu vejo um toast de erro com a mensagem {string}", (message: string) => {
+  cy.get(".Toastify__toast-body").should("contain.text", message);
+});
+
+Then("eu sou redirecionado para a página {string}", (page: string) => {
+  cy.wait(3000);
+  cy.url().should("include", page);
+});
+Then("eu continuo na página {string}", (page: string) => {
+  cy.url().should("include", page);
+});
